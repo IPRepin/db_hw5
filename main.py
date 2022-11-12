661db81681cbfcd71efabf56664d2894b1aa714f
Pavel Repin <sghcd2012@yandex.ru>
2022-11-12 16:38:39 +0300

    update

--- main.py
+++ main.py
@@ -12,23 +12,34 @@ def create_table():
     try:
         cur.execute("""
                         CREATE TABLE IF NOT EXISTS clients(
-                            clients_id SERIAL PRIMARY KEY,
+                            client_id SERIAL PRIMARY KEY,
                             first_name VARCHAR(60) NOT NULL
                             last_name VARCHAR(60) NOT NULL
-                            "e-mail" varchar NOT NULL UNIQUE CHECK ("e-mail" ILIKE '%@%.%' AND "e-mail" NOT LIKE '%@%@%')
-                            tel integer REFERENCES telephone(telephone_id)
+                            e_mail VARCHAR NOT NULL UNIQUE CHECK (e_mail LIKE '%@%.%' AND "e-mail" NOT LIKE '%@%@%')
                         );
                         """)
         cur.execute("""
                         CREATE TABLE IF NOT EXISTS telephone(
                             telephone_id SERIAL PRIMARY KEY,
+                            client_id INTEGER REFERENCES clients(client_id),
                             number char(10) UNIQUE CHECK (number SIMILAR TO '[0-9]{10}'),
                         );
                         """)
         conn.commit()
         print('Таблицы созданы успешно.')
-    except Exception as error:
+    except psycopg2.OperationalError as error:
         print(f"Произошла ошибка '{error}'")
+
+def add_client(client_id, first_name, last_name, e_mail):
+    cur.execute("""
+            INSERT INTO clients(first_name, last_name, e_mail, number) VALUES (%s, %s, %s, %s);
+            """, (client_id, first_name, last_name, e_mail,))
+    conn.commit()
+    cur.execute("""SELECT * FROM customers;""")
+    print(cur.fetchall())
+
+def add_namber(number):
+    pass
 
 
 
@@ -36,7 +47,11 @@ def create_table():
 
 
 def main():
-    pass
+    create_table()
+    first_name = input('Имя клиента: ')
+    last_name = input("Фамилия клиента: ")
+    e_mail = input("Email клиента: ")
+    add_client(first_name=first_name, last_name=last_name, e_mail=e_mail)
 
 if __name__ == "__main__":
     main()
